9cefac838564dfc2506d749636118347951e05c6
Nick Pocock <pocockn@hotmail.co.uk>
2016-05-12 21:02:29 +0100

    new clients template, updated styles

---
+++ clients.php
@@ -0,0 +1,94 @@
+<?php
+
+/**
+ * Template Name: Clients
+ */
+
+get_header();
+?>
+
+<div class="container">
+	<div class="row">
+		<div class="col-sm-12">
+			<h2 class="blue-header">Clients</h2>
+			<p style="margin-bottom:30px;">
+				With over 10,000 attendees expected at the show this year, have a look below at a small snap shot of who will be at the event. We add on more every week so check back regularly for updates…
+			</p>
+		</div>
+	</div>
+</div>
+
+<div class="container">
+	<div class="row">
+		<div class="col-md-3">
+			<button type="button" class="btn btn-primary btn-lg btn-block">
+				Affordable Housing
+			</button>
+			<button type="button" class="btn btn-primary btn-lg btn-block">
+				Education
+			</button>
+			<button type="button" class="btn btn-primary btn-lg btn-block">
+				Food Retail
+			</button>
+			<button type="button" class="btn btn-primary btn-lg btn-block">
+				Healthcare
+			</button>
+			<button type="button" class="btn btn-primary btn-lg btn-block">
+				Hotels
+			</button>
+			<button type="button" class="btn btn-primary btn-lg btn-block">
+				Industrial
+			</button>
+		</div>
+
+		<div class="col-md-9 attendees-page">
+			<ul id="attendees-page-list" class="text-center">
+				<li class="attendees-page-list-li" style="height: 60px;">
+					<img src="https://iotworldevent.com/files/117823-250x100-Ross_Logo.jpg" alt="Ross Stores" />
+					<p class="text-center">
+						<span></span>
+						Ross
+					</p>
+				</li>
+				<li class="attendees-page-list-li" style="height: 60px;">
+					<img src="https://iotworldevent.com/files/117803-250x100-Logitech_logo.jpg" alt="Logitech" />
+					<p class="text-center">
+						<span></span>
+						Logitech
+					</p>
+				</li>
+				<li class="attendees-page-list-li" style="height: 60px;">
+					<img src="https://iotworldevent.com/files/117783-250x100-GA_logo.jpg" alt="General Assembly" />
+					<p class="text-center">
+						<span></span>
+						General Assembly
+					</p>
+				</li>
+				<li class="attendees-page-list-li" style="height: 60px;">
+					<img src="https://iotworldevent.com/files/117753-250x100-EMC_logo.jpg" alt="EMC" />
+					<p class="text-center">
+						<span></span>
+						EMC
+					</p>
+				</li>
+				<li class="attendees-page-list-li" style="height: 60px;">
+					<img src="https://iotworldevent.com/files/117723-250x100-EastWest_Bank.jpg" alt="East West Bank" />
+					<p class="text-center">
+						<span></span>
+						East West Bank
+					</p>
+				</li>
+				<li class="attendees-page-list-li" style="height: 60px;">
+					<img src="https://iotworldevent.com/files/3453-250x100-United_States_Department_of_Defense_Seal.svg.jpg" alt="U.S. Department of Defense" />
+					<p class="text-center">
+						<span></span>
+						U.S. Department of Defense
+					</p>
+				</li>
+			</ul>
+		</div>
+	</div>
+</div>
+
+<?php get_footer();
+?>
