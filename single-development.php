777e12e04ec3117b170b430781700308f208d78a
Nick Pocock <pocockn@hotmail.co.uk>
2016-05-19 19:40:58 +0100

    updated project styles and single project mobile view

--- single-development.php
+++ single-development.php
@@ -4,6 +4,7 @@
  */
 
 get_header();
+
 ?>
 <hr>
  <?php
@@ -13,17 +14,30 @@ get_header();
 	$id = get_the_ID();
 
     ?>
-<div class="container">
+<div class="container hidden-md hidden-lg">
 	<div class="row">
-		<div class="col-md-3">
+		<div class="col-sm-12">
+			<?php $image = get_field('single_project_image', $id ); ?>
+			<img src="<?php echo $image[url]; ?>" class="img-responsive margin-image pull-right">
+			
+		</div>
+		<div class="col-sm-12">
 			<h2 class="blue-header"><?php the_title(); ?></h2>
 			<p class="first-para"><?php the_content(); ?></p>
 		</div>
-		<div class="col-md-9">
+	</div>
+</div>
+<div class="container hidden-sm hidden-xs">
+	<div class="row">
+		<div class="col-md-12">
 			<?php $image = get_field('single_project_image', $id ); ?>
-			<img src="<?php echo $image[url]; ?>" class="img-responsive margin-image">
+			<img src="<?php echo $image[url]; ?>" class="img-responsive margin-image pull-right" style="max-width: 70%;">
+			<h2 class="blue-header"><?php the_title(); ?></h2>
+			<p class="first-para"><?php the_content(); ?></p>
 		</div>
 	</div>
 </div>
+
 <?php endwhile; ?>
+
 <?php get_footer(); ?>
