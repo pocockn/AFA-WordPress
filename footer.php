f124dbcd04a0989d658893e99fc00af084c3e1d3
Nick Pocock <nick.pocock@informa.com>
2016-05-20 08:26:41 +0100

    added footer styles, added bpw text into footer, footer sitemap

--- footer.php
+++ footer.php
@@ -1,13 +1,53 @@
- <hr>
-    <div class="row">
-      <div class="container">
-        <div class="col-md-12">
-          <footer>
-            <p>&copy; AFA 2016</p>
-          </footer>
+
+<?php
+/**
+ * The template for displaying the footer
+ *
+ * Contains footer content and the closing of the #body div element.
+ */
+?>
+    <footer class="spacer">
+        <div class="container">
+            <div class="row">
+                <div class="col-sm-5">
+                    <h4>AFA</h4>
+                    <p>A firm based in Cirencester</p>
+                </div>
+
+                <div class="col-sm-3">
+                    <h4>Quick Links</h4>
+                    <?php /* Primary navigation */
+                        wp_nav_menu( array(
+                          'menu' => 'top_menu',
+                          'depth' => 2,
+                          'container' => false,
+                          'menu_class' => 'list-unstyled',
+                          //Process nav menu using our custom nav walker
+                          'walker' => new wp_bootstrap_navwalker())
+                        );
+                    ?>
+                </div>
+                <div class="col-sm-4 subscribe">
+                    <h4>Keep up to date with the latest news</h4>
+                    <div class="input-group">
+						 [text* email-address class:form-control placeholder "Email"]
+						 <span class="input-group-btn">
+						 [submit class:btn class:btn-default "Subscribe"]
+						 </span>
+					</div>
+                </div>
+            </div>
+            <!--/.row-->
         </div>
-      </div>
+        <!--/.container-->
+
+        <!--/.footer-bottom-->
+    </footer>
+    <!-- end of footer section -->
+    <div class="text-center copyright">Site by Blueprint Web Designs</a>
     </div>
+
 	<?php wp_footer(); ?>
-  </body>
+	
+</body>
 </html>
